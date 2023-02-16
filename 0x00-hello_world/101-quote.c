e9da896566d5e354b0e4a8de8c152d7657aa2cc8
dani <dani@gmail.com>
2023-02-17 01:00:21 +0300

    100-intel

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -4,7 +4,7 @@
  * main - A c program that prints a line to the standard error
  * Return 1 (Success)
  */
-int main(void)
+void main()
 {
 	write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 return (1);
