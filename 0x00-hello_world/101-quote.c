9e1ebe35447564a55c9703b64a0b337962404b72
dani <dani@gmail.com>
2023-02-17 00:33:14 +0300

    100-intel

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -6,6 +6,6 @@
  */
 int main(void)
 {
-	fputs('and that piec of art is useful" - Dora Korpar, 2015-10-19\n', stdout);
+	fputs("and that piec of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
 return (1);
 }
