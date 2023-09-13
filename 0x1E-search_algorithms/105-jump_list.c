b4d46342d25e2bfc0e94ef8ff02884c3f84b9209
Daniel <daniel@gmail.com>
2023-09-13 15:47:21 +0300

    105-jump_list.c

--- 0x1E-search_algorithms/105-jump_list.c
+++ 0x1E-search_algorithms/105-jump_list.c
@@ -18,7 +18,7 @@ listint_t *jump_list(listint_t *list, size_t size, int value)
 	if (list == NULL || size == 0)
 		return (NULL);
 
-	m = (size_t)sqrt((double)size);
+	j = (size_t)sqrt((double)size);
 	index = 0;
 	i = 0;
 
